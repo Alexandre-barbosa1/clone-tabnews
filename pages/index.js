b4b0813893a067607a9aafaca096d597835eb51f
Alexandre <josealexandre2005@outlook.com>
2026-08-18 14:48:38 +0000

    adicionando uma nova mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home(){
-    return <h1>continue assim, voce é top e esta indo bem!! Apesar dos 
-        apesares e todo os desafios voce continua firme e forte</h1>
+    return <h1>Volte aqui as 22:30</h1>
 }
 
 export  default Home;
